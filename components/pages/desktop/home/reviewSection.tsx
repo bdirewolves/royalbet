d90224adba14cd749d5621eef4296cedef9c237f
bank.tdev <bank.tdev@gmail.com>
2023-02-10 18:58:40 +0700

    Bug Index

--- components/pages/desktop/home/reviewSection.tsx
+++ components/pages/desktop/home/reviewSection.tsx
@@ -6,7 +6,7 @@ import "slick-carousel/slick/slick-theme.css";
 
 export default function ReviewSection() {
     const settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
