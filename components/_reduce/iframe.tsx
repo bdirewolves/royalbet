f269c961e81842613d8412164be98899fa9449d1
banktdev <bank.tdev@gmail.com>
2023-03-30 17:12:21 +0700

    border iframe

--- components/_reduce/iframe.tsx
+++ components/_reduce/iframe.tsx
@@ -45,6 +45,8 @@ export default function Iframe({path, title, param}: IProps){
 }
 
 const Ifr = styled.iframe`
+    border: 1px solid #fff;
+    border-radius: 10px;
     width: 100%;
     height: 100%;
 `
